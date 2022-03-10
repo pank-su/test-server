922f74746e889c750511f8f1e4ed98f1f777b9f2
user <frostkslo1@yandex.ru>
2022-03-10 11:58:11 +0300

    Добавлена функция, которая выыводит файл, она тестовая и не доделана(check TODO)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser");
+const fs = require("fs")
 
 const app = express()
 
@@ -18,5 +19,14 @@ app.post("/", function (request, response) { // Тестовый post-запро
     console.log(request.body)
     response.send("ok")
 })
-
+app.get("/get_file/:filename", function (request, response) { // Функция для получения файлов
+    // TODO Надо обезопасить данную функцию и сделать обработку ошибок
+    console.log(request.params.filename)
+    var file = fs.createReadStream('./' + request.params.filename);
+    var stat = fs.statSync('./' + request.params.filename);
+    response.setHeader('Content-Length', stat.size);
+    response.setHeader('Content-Type', 'application/pdf');
+    response.setHeader('Content-Disposition', 'attachment; filename=' + request.params.filename);
+    file.pipe(response);
+})
 app.listen(3000, "localhost")
